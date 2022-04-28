1dedd30d7f0d8504d85c05b397e5ea65b2c8f04c
seungmin-dev <seungmin.dev@gmail.com>
2022-04-28 11:55:38 +0900

    css modules - 1

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,15 +1,15 @@
 import Link from "next/link"
 import { useRouter } from "next/router"
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
     const router = useRouter();
-    return <nav>
+    return <nav className={styles.nav}>
         <Link href="/">
-            <a style={{color: router.asPath === "/" ? "red" : "blue"}}>Home</a>
+            <a>Home</a>
         </Link>
-        {/* Link는 단지 href를 위해서 씀. style, className 등은 a 태그에 작성 */}
         <Link href="/about">
-            <a style={{color: router.asPath === "/about" ? "red" : "blue"}}>About</a>
+            <a>About</a>
         </Link>
     </nav>
 }
