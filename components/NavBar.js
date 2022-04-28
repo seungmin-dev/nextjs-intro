488ec7f7f22fd0cc78a7f12b938acac084db150b
seungmin-dev <seungmin.dev@gmail.com>
2022-04-28 12:54:59 +0900

    styles jsx

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,10 +6,23 @@ export default function NavBar() {
     const router = useRouter();
     return <nav>
         <Link href="/">
-            <a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>Home</a>
+            <a className={router.pathname === "/" ? "active" : ""}>Home</a>
         </Link>
         <Link href="/about">
-            <a className={[styles.link, router.pathname === "/about" ? styles.active : "",].join(" ")}>About</a>
+            <a className={router.pathname === "/about" ? "active" : ""}>About</a>
         </Link>
+        <style jsx>{`
+            nav {
+                background-color: tomato;
+            }
+            a {
+                text-decoration: none;
+            }
+            .active {
+                color: white;
+            }
+            /* 컴포넌트 별로 스타일 지정, 태그 이름 그대로 사용해도 무방함 */
+            /* 부모 컴포넌트가 같은 클래스 이름을 사용하고 있어도 상관없음 */
+        `}</style>
     </nav>
 }
