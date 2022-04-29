86e36cccb9094ebe98a4bdc946a9b7876133ac18
seungmin-dev <seungmin.dev@gmail.com>
2022-04-29 16:21:25 +0900

    fetching data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "14754f6777692014937322454e829a50";
+
 export default function Home() {
+    const [movies, setMovies] = useState([]);
+    useState();
+   useEffect(() => {
+     (async () => {
+       const { results } = await (
+         await fetch(
+           `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+         )
+       ).json();
+       setMovies(results);
+     })();
+   }, []);
     return (
         <div>
             <Seo title="Home" />
-            <h1>Hello</h1>
+            {!movies && <h4>Loading...</h4>}
+            {movies.map(movie => <div key={movie.id}>
+                <h4>{movie.original_title}</h4>
+                </div>)}
         </div>
     )
 }
