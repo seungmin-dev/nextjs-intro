8aed2a6728872732d4628391a1e5def96b96aa94
seungmin-dev <seungmin.dev@gmail.com>
2022-05-01 00:04:53 +0900

    detail page - router.push

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,46 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Seo from "../components/Seo";
 
-export default function Home({results}) {
-   return (
+export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title,
+        },
+      },
+      `movies/${id}`
+    );
+    // div를 클릭했을 때 괄호 안의 주소로 이동시켜줌
+  };
+  return (
     <div className="container">
       <Seo title="Home" />
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
-        </div> ))}
+          <Link
+            href={{
+              pathname: `/movies/${movie.id}`,
+              query: {
+                title: movie.original_title,
+              },
+            }}
+            as={`/movies/${movie.id}`}
+          >
+            <a>
+              <h4>{movie.original_title}</h4>
+            </a>
+          </Link>
+        </div>
+      ))}
       <style jsx>{`
         .container {
           display: grid;
@@ -17,7 +49,7 @@ export default function Home({results}) {
           gap: 20px;
         }
         .Movie {
-            cursor: pointer;
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
@@ -39,18 +71,11 @@ export default function Home({results}) {
 
 // 다른 함수 명들은 내 마음대로 해도 되지만 getServerSideProps 는 이 이름 그대로 해야함!!
 export async function getServerSideProps() {
-    const res = await fetch(`http://localhost:3000/api/movies`);
-    const {results} = await res.json();
-    return {
-      props: {
-        results,
-      },
-    };
-  }
-// 위에 작성하는 코드는 server에서 작동함!(X client X)
-// API KEY를 여기에 작성해도 됨
-// server에서 렌더링을 하면 소스코드에 렌더된 results를 확인할 수 있음!(HTML)
-// Loading 화면을 따로 안넣어도 되지만 렌더되기 전까지는 빈 화면
-// js가 비활성화 되어있더라도 작동할 것!(서버에서 작동하는 것이기 때문에)
-
-// 같은 번호로 포트가 열려있으면 getServerSideProps가 제대로 작동하지 않음!
+  const res = await fetch(`http://localhost:3000/api/movies`);
+  const { results } = await res.json();
+  return {
+    props: {
+      results,
+    },
+  };
+}
--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -2,8 +2,11 @@ import { useRouter } from "next/router";
 
 export default function Detail() {
     const router = useRouter();
-    console.log(router);
-    return "detail";
+    return (
+        <div>
+            <h4>{router.query.title || "Loading..."}</h4>
+        </div>
+    );
 }
 
 // 변수명이 포함된 경로로 이동하고 싶다면 그 변수 명을 파일명에 [] 안에 넣어서 만듦
